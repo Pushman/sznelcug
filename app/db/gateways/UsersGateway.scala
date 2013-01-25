7f6e8172c73dd0d39a597377ab26a0993f8eac64
Pushman <pushman.pl@gmail.com>
2013-01-25 20:24:33 +0100

    Usunięcie zbędnej metody z UsersGateway

--- app/db/gateways/UsersGateway.scala
+++ app/db/gateways/UsersGateway.scala
@@ -3,10 +3,7 @@ package db.gateways
 import domain.models.User
 import concurrent.Future
 
-
 trait UsersGateway {
-
-  def findUserOption(user: User): Future[Option[User]]
 
   def findUserBy(username: String, password: String): Future[Option[User]]
 }
--- app/db/gateways/impl/UsersGatewayImpl.scala
+++ app/db/gateways/impl/UsersGatewayImpl.scala
@@ -1,7 +1,5 @@
 package db.gateways.impl
 
-import concurrent.Future
-import domain.models.User
 import db.gateways.UsersGateway
 import db.gateways.helpers.FetchAsync
 import org.squeryl.PrimitiveTypeMode._
@@ -10,10 +8,6 @@ class UsersGatewayImpl extends UsersGateway {
 
   import play.api.Play.current
   import db.gateways.tables.UserSchema._
-
-  override def findUserOption(user: User): Future[Option[User]] = FetchAsync {
-    users.where(u => u.username === user.username and u.password === user.password).headOption
-  }
 
   def findUserBy(username: String, password: String) = FetchAsync {
     users.where(u => u.username === username and u.password === password).headOption
