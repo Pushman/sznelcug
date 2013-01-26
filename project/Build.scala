98c694ef45a2beb89daead4c490f9a58bd47d80f
Pushman <pushman.pl@gmail.com>
2013-01-26 23:02:57 +0100

    Autoryzacja użytkownika oparta na aktorach Działa poza UpdateUser

--- project/Build.scala
+++ project/Build.scala
@@ -4,6 +4,9 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
+  import Versions._
+  import Resolvers._
+
   val appName = "sznelcug"
   val appVersion = "0.1-SNAPSHOT"
 
@@ -19,10 +22,23 @@ object ApplicationBuild extends Build {
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     lessEntryPoints <<= baseDirectory(customLessEntryPoints),
+    resolvers := Seq(eligosource),
     libraryDependencies ++= List(
       "org.slf4j" % "slf4j-nop" % "1.6.4",
       "com.h2database" % "h2" % "1.3.166",
-      "org.squeryl" % "squeryl_2.10.0-RC1" % "0.9.5-4"
+      "org.squeryl" % "squeryl_2.10.0-RC1" % squeryl,
+      "org.eligosource" %% "eventsourced" % eventsourced % "compile"
     )
   )
+}
+
+object Versions {
+
+  lazy val eventsourced = "0.5-M1"
+  lazy val squeryl = "0.9.5-4"
+}
+
+object Resolvers {
+
+  lazy val eligosource = "Eligosource Releases Repo" at "http://repo.eligotech.com/nexus/content/repositories/eligosource-releases/"
 }
