9d09406f12827c35f6c0d70e95dca437c292bbb4
Pushman <pushman.pl@gmail.com>
2013-01-31 17:42:24 +0100

    Refaktoring konfiguracji

--- project/Build.scala
+++ project/Build.scala
@@ -24,12 +24,12 @@ object ApplicationBuild extends Build {
     lessEntryPoints <<= baseDirectory(customLessEntryPoints),
     resolvers := Seq(eligosource),
     libraryDependencies ++= List(
-      "org.slf4j" % "slf4j-nop" % "1.6.4",
       "com.h2database" % "h2" % "1.3.166",
       "org.squeryl" % "squeryl_2.10.0-RC1" % squeryl,
-      "com.typesafe.akka" %% "akka-testkit" % akka,
-      "org.eligosource" %% "eventsourced" % eventsourced,
-      "org.scalatest" %% "scalatest" % scalatest % "test"
+      "com.typesafe.akka" %% "akka-actor" % akka withSources(),
+      "com.typesafe.akka" %% "akka-testkit" % akka withSources(),
+      "org.eligosource" %% "eventsourced" % eventsourced withSources(),
+      "org.scalatest" %% "scalatest" % scalatest % "test " withSources()
     )
   )
 }
