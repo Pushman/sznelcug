eb85c074ba64f831e50e8a04e1b2b9f821ebd27d
Pushman <pushman.pl@gmail.com>
2013-01-31 17:14:14 +0100

    Rezygnacja z ChildDispatchera

--- app/services/actors/AuthenticationActor.scala
+++ app/services/actors/AuthenticationActor.scala
@@ -3,38 +3,39 @@ package services.actors
 import akka.actor.{ActorRef, Actor}
 import java.util.UUID
 import domain.models.User
-import helpers.ChildDispatcher
 import concurrent.duration._
 
-class AuthenticationActor extends Actor with ChildDispatcher {
+import akka.pattern.ask
+
+class AuthenticationActor extends Actor {
   provider: Actor with ActorProvider =>
+
+  import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
   val usersReadActor = provider.actorFor(classOf[UsersReadModelActor])
   val usersWriteActor = provider.actorFor(classOf[UsersWriteModelActor])
-  implicit val timeout = (5 seconds)
+  implicit val timeout = akka.util.Timeout(5 seconds)
 
-  override def receive = dispatchToChild(AuthenticationActorChild.apply)
-
-  case class AuthenticationActorChild(replyTo: ActorRef) extends Actor {
-
-    override def receive = {
-      case AuthorizationCommand(UsernamePasswordToken(username, password)) =>
-        usersReadActor ! ReadUser(UserLookup(username, password))
-
-      case UserFound(user) =>
-        usersWriteActor ! UpdateUser(newSessionKeyFor(user))
-
-      case UserNotFound(UserLookup(username, password)) =>
-        replyTo ! AuthorizationFailure()
-
-      case UserUpdated(u) =>
-        replyTo ! AuthorizationSuccess(UserCredentials(u.sessionKey))
-    }
-
-    def newSessionKeyFor(user: User) =
-      user.update(sessionKey = UUID.randomUUID().toString)
+  override def receive = {
+    case AuthorizationCommand(UsernamePasswordToken(username, password)) =>
+      usersReadActor ? ReadUser(UserLookup(username, password)) collect readUser(sender)
   }
 
+  private def readUser(replyTo: ActorRef): Receive = {
+    case UserNotFound(_) =>
+      replyTo ! AuthorizationFailure()
+
+    case UserFound(user) =>
+      usersWriteActor ? UpdateUser(newSessionKeyFor(user)) collect userUpdated(replyTo)
+  }
+
+  private def userUpdated(replyTo: ActorRef): Receive = {
+    case UserUpdated(user) =>
+      replyTo ! AuthorizationSuccess(UserCredentials(user.sessionKey))
+  }
+
+  private def newSessionKeyFor(user: User) =
+    user.update(sessionKey = UUID.randomUUID().toString)
 }
 
 trait AuthenticationToken
